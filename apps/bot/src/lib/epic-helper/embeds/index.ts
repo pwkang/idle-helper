c6aa98ff767ce89afe4e7fa573b0accc0855e154
pwkang <porweikang@hotmail.com>
2023-07-24 23:04:44 +0800

    refactor: remove unused files

--- apps/bot/src/lib/epic-helper/embeds/index.ts
+++
@@ -1,9 +0,0 @@
-import _howToRegisterEmbed from './how-to-register.embed';
-import _turnOnAccount from './turn-on-account';
-
-const embedProvider = {
-  howToRegister: _howToRegisterEmbed,
-  turnOnAccount: _turnOnAccount,
-};
-
-export default embedProvider;
