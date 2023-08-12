134e4522de3154a6ce2e1d9650126acd44e27477
pwkang <porweikang@hotmail.com>
2023-08-12 23:19:55 +0800

    chore: rename `status` to `lastclaim`

--- apps/bot/src/commands/prefix/idle-helper/farms/last-claim.ts
+++ apps/bot/src/commands/prefix/idle-helper/farms/last-claim.ts
@@ -1,20 +1,16 @@
-import {
-  PREFIX_COMMAND_TYPE,
-  USER_ACC_OFF_ACTIONS,
-  USER_NOT_REGISTERED_ACTIONS,
-} from '@idle-helper/constants';
+import {PREFIX, PREFIX_COMMAND_TYPE, USER_ACC_OFF_ACTIONS, USER_NOT_REGISTERED_ACTIONS} from '@idle-helper/constants';
 import commandHelper from '../../../../lib/idle-helper/command-helper';
 import {djsMessageHelper} from '../../../../lib/discordjs/message';
 
 export default <PrefixCommand>{
-  name: 'userStatus',
-  commands: ['status'],
+  name: 'userLastClaim',
+  commands: ['lastClaim', 'lc', 'status'],
   type: PREFIX_COMMAND_TYPE.bot,
   preCheck: {
     userNotRegistered: USER_NOT_REGISTERED_ACTIONS.askToRegister,
     userAccOff: USER_ACC_OFF_ACTIONS.askToTurnOn,
   },
-  execute: async (client, message) => {
+  execute: async (client, message, args) => {
     const status = await commandHelper.farms.status({
       author: message.author,
     });
@@ -24,5 +20,14 @@ export default <PrefixCommand>{
       channelId: message.channelId,
       options: status.render(),
     });
+    if (args.includes('status')) {
+      await djsMessageHelper.send({
+        client,
+        channelId: message.channelId,
+        options: {
+          content: `This command has been changed to \`${PREFIX.bot}lastclaim\` or \`${PREFIX.bot}lc\`\n\`${PREFIX.bot}status\` will be removed in few days.`,
+        },
+      });
+    }
   },
 };
