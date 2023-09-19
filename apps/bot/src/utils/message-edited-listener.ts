ab1414871f0f06308275752d53929b09eacc960d
Pwkang <porweikang@hotmail.com>
2023-09-19 20:43:27 +0800

    fix: resolve memory leak (#61)

--- apps/bot/src/utils/message-edited-listener.ts
+++ apps/bot/src/utils/message-edited-listener.ts
@@ -1,19 +1,31 @@
 import {Message} from 'discord.js';
 import {redisMessageEdited} from '../services/redis/message-edited.redis';
 import {EventEmitter} from 'events';
+import ms from 'ms';
 
 const messageEditedEvent = new EventEmitter();
 
 interface ICreateMessageEditedListener {
   messageId: string;
+  timeout?: number;
 }
 
 export const createMessageEditedListener = async ({
   messageId,
+  timeout = ms('10m'),
 }: ICreateMessageEditedListener) => {
   await redisMessageEdited.register({
     messageId,
   });
+
+  messageEditedEvent.on = (messageId: string | symbol, callback: (message: Message) => void): any => {
+
+    setTimeout(() => {
+      messageEditedEvent.removeListener(messageId, callback);
+    }, timeout);
+
+    return messageEditedEvent.addListener(messageId, callback);
+  };
 
   return messageEditedEvent;
 };
@@ -25,4 +37,21 @@ export const emitMessageEdited = async (message: Message) => {
   });
   if (!isEdited) return;
   messageEditedEvent.emit(messageId, message);
+};
+
+interface IRemoveMessageEditedListener {
+  messageId: string;
+  callback: (message: Message) => void;
+  timeout?: number;
+}
+
+export const removeMessageEditedListener = async ({
+  timeout = ms('10m'),
+  messageId,
+  callback,
+}: IRemoveMessageEditedListener) => {
+  setTimeout(() => {
+
+    messageEditedEvent.removeListener(messageId, callback);
+  }, timeout);
 };
