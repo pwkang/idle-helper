0051c19c301975aa1f33994258b4236abb71360a
Pwkang <porweikang@hotmail.com>
2023-10-01 01:44:09 +0800

    chore: some bugs fix (#83)

--- apps/bot/src/utils/getTop3Power.ts
+++ apps/bot/src/utils/getTop3Power.ts
@@ -5,6 +5,7 @@ import {IUser} from '@idle-helper/models';
 export const getTop3Power = (userAccount: IUser) => {
   if (!userAccount.lastUpdated?.workers) return 0;
   const top3Workers = typedObjectEntries(userAccount.workers)
+    .filter(([, worker]) => worker)
     .map(([type, worker]) => ({
       power: calcWorkerPower({type, level: worker.level, decimalPlace: 3}),
     }))
