8b6531165d6cfa3be9a2ffe8fe92afc8fcc0a09f
pwkang <porweikang@hotmail.com>
2023-12-02 15:13:12 +0800

    fix: claim reader not reading second fields

--- apps/bot/src/lib/idle-farm/message-readers/claim.ts
+++ apps/bot/src/lib/idle-farm/message-readers/claim.ts
@@ -10,8 +10,8 @@ type TInventoryItems = Partial<Record<keyof typeof IDLE_FARM_ITEMS, number>>;
 
 export const _claimReader = ({embed}: IClaimReader) => {
   const items: TInventoryItems = {};
-  if (embed.fields[0].value) {
-    const itemsRow = embed.fields[0].value.split('\n');
+  for(const fields of embed.fields) {
+    const itemsRow = fields.value.split('\n');
     for (const itemRow of itemsRow) {
       const type = typedObjectEntries(IDLE_FARM_ITEMS).find(([, name]) =>
         itemRow.match(new RegExp(`\\*\\*${name}\\*\\*`))
